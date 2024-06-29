028786aa99395002feecbfa49d47f870c2273b28
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2024-06-29 22:26:59 +0800

    update to_selfies

--- released_box/to_selfies.py
+++ released_box/to_selfies.py
@@ -2,7 +2,7 @@
 
 import selfies
 import pandas
-df_sel = pandas.read_csv("E:/maintain/easy-pytorch/released_box/molecule.smi", sep='\t') # get smiles strings
+df_sel = pandas.read_csv("molecule.smi", sep='\t') # get smiles strings
 dataset = list(df_sel.iloc[:,0])
 for i, s in enumerate(dataset):
     sf = selfies.encoder(s)
@@ -10,7 +10,7 @@ for i, s in enumerate(dataset):
     df_sel.iloc[i,0] = sf
 alphabet = selfies.get_alphabet_from_selfies(dataset) # now selfies
 alphabet.add("[nop]")  # [nop] is a special padding symbol
-alphabet.add('.') # in selfies
+alphabet.add('.') # symbol different in selfies, be counted in len_selfies, discard in get_alphabet_from_selfies
 alphabet = list(sorted(alphabet))
 pad_to_len = max(selfies.len_selfies(s) for s in dataset)
 symbol_to_idx = {s: i for i, s in enumerate(alphabet)}
