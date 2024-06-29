99ed9c36c015103ecd5ade6fdf92c32136df4108
Jing Lin <82669431+linjing-lab@users.noreply.github.com>
2024-06-29 22:23:28 +0800

    add to_selfies

---
+++ released_box/to_selfies.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2024 linjing-lab
+
+import selfies
+import pandas
+df_sel = pandas.read_csv("E:/maintain/easy-pytorch/released_box/molecule.smi", sep='\t') # get smiles strings
+dataset = list(df_sel.iloc[:,0])
+for i, s in enumerate(dataset):
+    sf = selfies.encoder(s)
+    dataset[i] = sf
+    df_sel.iloc[i,0] = sf
+alphabet = selfies.get_alphabet_from_selfies(dataset) # now selfies
+alphabet.add("[nop]")  # [nop] is a special padding symbol
+alphabet.add('.') # in selfies
+alphabet = list(sorted(alphabet))
+pad_to_len = max(selfies.len_selfies(s) for s in dataset)
+symbol_to_idx = {s: i for i, s in enumerate(alphabet)}
+for s in dataset:
+    label, one_hot = selfies.selfies_to_encoding(
+    selfies=s,
+    vocab_stoi=symbol_to_idx,
+    pad_to_len=pad_to_len,
+    enc_type="both"
+    )
+    print(s, label) # or append [label, one_not] into df_sel named descriptor_selfies
+df_sel.to_csv('molecule.selfies', sep='\t', index=False, header=False)
